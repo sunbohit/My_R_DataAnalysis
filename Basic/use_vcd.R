7415949755a0ff6ae7b63899385173488868c2f7
sunbohit <a824834@163.com>
2017-02-20 16:11:34 +0800

    使用vcd作图并保存

--- Basic/use_vcd.R
+++ Basic/use_vcd.R
@@ -7,6 +7,6 @@ help(package="vcd") #打开vcd包的帮助文档
 library(vcd) #载入vcd包
 help(Arthritis) #打开Arthritis的描述。Arthritis显示了接收治疗的关节炎患者与接收安慰剂治疗的关节炎患者间的比较数据
 Arthritis #显示数据
-jpeg(file="H:/R/My_R_DataAnalysis/Basic/Arthritis.jpeg")
-example(Arthritis)
-dev.off()
+jpeg(file="Arthritis.jpeg") #保存至图片
+example(Arthritis) #显示分析样例
+dev.off() #将输出返回到终端
