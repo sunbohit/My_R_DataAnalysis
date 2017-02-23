6f3dfd56548f73f91afc28dd7dea32b36afe20fb
sunbohit <a824834@163.com>
2017-02-23 16:37:06 +0800

    添加键盘输入数据框

--- DataStructure/data_io.R
+++ DataStructure/data_io.R
@@ -1,5 +1,8 @@
+#R语言中的数据的输入操作
+
 setwd('H:/R/My_R_DataAnalysis/DataStructure') #修改工作空间
 sink('data_io.result', append=TRUE, split=TRUE) #输出重定向
+
+# 通过数据框，用键盘输入数据
 mydata <- data.frame(age=numeric(0), gender=character(0), weight=numeric(0))
 mydata <- edit(mydata)
-q()
