f74c21f81581a693b3b7817fbb04861dc43503b5
sunbohit <a824834@163.com>
2017-03-02 15:08:07 +0800

    更新了作图

--- DrawGraphBasic/draw_graph_basic.R
+++ DrawGraphBasic/draw_graph_basic.R
@@ -16,7 +16,22 @@ dev.off()
 dose  <- c(20, 30, 40, 45, 60)
 drugA <- c(16, 20, 27, 40, 60)
 drugB <- c(15, 18, 25, 31, 40)
-dev.new()
+#dev.new()
 jpeg("draw_graph_basic_2.jpg")
 plot(dose, drugA, type="b") #作图，type="b"表示同时绘制点和线
+dev.off()
+
+#修改图像参数
+#dev.new()
+opar <- par(no.readonly=TRUE) # 备份当前图形参数列表
+par(lty=2, pch=17) # lty=2 代表虚线 ； pch=17 代表实心三角形
+jpeg("draw_graph_basic_3.jpg")
+plot(dose, drugA, type="b")   #作图 
+par(opar)                     #重新载入原始设置 
+dev.off()
+
+#修改单个图像的参数
+dev.new()
+jpeg("draw_graph_basic_4.jpg")
+plot(dose, drugA, type="b", lty=3, lwd=3, pch=15, cex=2)
 dev.off()
