944bbac8dfcdaece5188c5684ca5855762e641a3
sunbohit <a824834@163.com>
2017-03-03 22:19:23 +0800

    基本数据管理

---
+++ DataManagement/data_management.R
@@ -0,0 +1,17 @@
+#R语言中的常用数据结构
+
+setwd('H:/R/My_R_DataAnalysis/DataManagement') #修改工作空间
+sink('data_management.result', append=TRUE, split=TRUE) #输出重定向
+
+# 创建数据框
+manager <- c(1,2,3,4,5)
+date <- c("10/24/08","10/28/08","10/1/08","10/12/08","5/1/09")
+gender <- c("M","F","F","M","F")
+age <- c(32,45,25,39,99)
+q1 <- c(5,3,3,3,2)
+q2 <- c(4,5,5,3,2)
+q3 <- c(5,2,5,4,1)
+q4 <- c(5,5,5,NA,2)
+q5 <- c(5,5,2,NA,1)
+leadership <- data.frame(manager,date,gender,age,q1,q2,q3,q4,q5, 
+                         stringsAsFactors=FALSE)
